f4d856a93c6c36e2994d1b10e41130edb6418c07
Andriy <andriybojkoo@gmail.com>
2020-05-04 12:43:26 +0300

    Initial commit

---
+++ OpenWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OpenWeather
+//
+//  Created by MineDest on 5/4/20.
+//  Copyright © 2020 MineDest. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
